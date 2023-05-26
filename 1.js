2d5e0b52657289fc2a741845438c3dac2d83dee2
Sunjcfly123 <2933463490@qq.com>
2023-05-26 18:48:25 +0800

    diyigebanben

---
+++ 1.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         xiaoxiaoniao
+// @namespace    http://chrxw.com/
+// @version      0.1
+// @description  测试
+// @author       我是一只小小鸟
+// @match        http://*/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @license           AGPL License
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     cnosole.log('wolsdjflk');
+})();
